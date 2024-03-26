e0b659ddd4aac9052701625a82547d5ca91fbdaa
frisck <55840140+nsilvan1@users.noreply.github.com>
2024-03-26 12:29:53 -0300

    carro

---
+++ resources/[carros]/Creative_Vehs/fxmanifest.lua
@@ -0,0 +1,137 @@
+fx_version "bodacious"
+game "gta5"
+
+client_script {
+    'names.lua'
+}
+
+files {
+	"data/*",
+	"audio/*",
+	"audio/**/*"
+}
+
+data_file "CARCOLS_FILE" "data/carcols.meta"
+data_file "POPSCHED_FILE" "data/popcycle.dat"
+data_file "HANDLING_FILE" "data/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+
+data_file "AUDIO_WAVEPACK" "audio/lfasound/dlc_lfasound"
+data_file "AUDIO_GAMEDATA" "audio/lfasound/lfasound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/lfasound/lfasound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r35sound/dlc_r35sound"
+data_file "AUDIO_GAMEDATA" "audio/r35sound/r35sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r35sound/r35sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r34sound/dlc_r34sound"
+data_file "AUDIO_GAMEDATA" "audio/r34sound/r34sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r34sound/r34sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/carrerav10/dlc_carrerav10"
+data_file "AUDIO_GAMEDATA" "audio/carrerav10/carrerav10_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/carrerav10/carrerav10_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/huracanv10/dlc_huracanv10"
+data_file "AUDIO_GAMEDATA" "audio/huracanv10/huracanv10_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/huracanv10/huracanv10_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/hemisound/dlc_hemisound"
+data_file "AUDIO_GAMEDATA" "audio/hemisound/hemisound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/hemisound/hemisound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/argento/dlc_argento"
+data_file "AUDIO_GAMEDATA" "audio/argento/argento_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/argento/argento_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/chevroletlt4/dlc_chevroletlt4"
+data_file "AUDIO_GAMEDATA" "audio/chevroletlt4/chevroletlt4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/chevroletlt4/chevroletlt4_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/gtr/sfx/dlc_gt3flat6"
+data_file "AUDIO_GAMEDATA" "audio/gtr/audioconfig/gt3flat6_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/gtr/audioconfig/gt3flat6_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
+data_file "AUDIO_GAMEDATA" "audio/elegyx/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/elegyx/elegyx_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/mercm177/dlc_mercm177"
+data_file "AUDIO_GAMEDATA" "audio/mercm177/mercm177_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/mercm177/mercm177_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/lambov10/dlc_lambov10"
+data_file "AUDIO_GAMEDATA" "audio/lambov10/lambov10_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/lambov10/lambov10_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/b58b30/dlc_b58b30"
+data_file "AUDIO_GAMEDATA" "audio/b58b30/b58b30_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/b58b30/b58b30_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/s63b44/dlc_s63b44"
+data_file "AUDIO_GAMEDATA" "audio/s63b44/s63b44_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/s63b44/s63b44_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/rotary7/dlc_rotary7"
+data_file "AUDIO_GAMEDATA" "audio/rotary7/rotary7_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/rotary7/rotary7_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/toysupmk4/dlc_toysupmk4"
+data_file "AUDIO_GAMEDATA" "audio/toysupmk4/toysupmk4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/toysupmk4/toysupmk4_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/f20c/dlc_f20c"
+data_file "AUDIO_GAMEDATA" "audio/f20c/f20c_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/f20c/f20c_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/rb26dett/dlc_rb26dett"
+data_file "AUDIO_GAMEDATA" "audio/rb26dett/rb26dett_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/rb26dett/rb26dett_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/predatorv8/dlc_predatorv8"
+data_file "AUDIO_GAMEDATA" "audio/predatorv8/predatorv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/predatorv8/predatorv8_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/K20/dlc_k20a"
+data_file "AUDIO_GAMEDATA" "audio/K20/k20a_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/K20/k20a_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/M5/dlc_m5cracklemod"
+data_file "AUDIO_GAMEDATA" "audio/M5/m5cracklemod_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/M5/m5cracklemod_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/muscle2/dlc_musv8"
+data_file "AUDIO_GAMEDATA" "audio/muscle2/musv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/muscle2/musv8_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/Exige12/dlc_m297zonda"
+data_file 'AUDIO_SYNTHDATA' 'audio/Exige12/m297zonda_amp.dat'
+data_file "AUDIO_GAMEDATA" "audio/Exige12/m297zonda_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/Exige12/m297zonda_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/678lt/dlc_shonen"
+data_file 'AUDIO_SYNTHDATA' 'audio/678lt/shonen_amp.dat'
+data_file "AUDIO_GAMEDATA" "audio/678lt/shonen_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/678lt/shonen_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/z33u2/dlc_z33u2"
+data_file "AUDIO_GAMEDATA" "audio/z33u2/z33u2_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/z33u2/z33u2_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/F136/dlc_F136"
+data_file "AUDIO_GAMEDATA" "audio/F136/F136_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/F136/F136_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/s15sound/dlc_s15sound"
+data_file "AUDIO_GAMEDATA" "audio/s15sound/s15sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/s15sound/s15sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/sentinelsg4/dlc_sentinelsg4"
+data_file "AUDIO_GAMEDATA" "audio/sentinelsg4/sentinelsg4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/sentinelsg4/sentinelsg4_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/lancer/dlc_sestov10"
+data_file "AUDIO_GAMEDATA" "audio/lancer/sestov10_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/lancer/sestov10_sounds.dat"
