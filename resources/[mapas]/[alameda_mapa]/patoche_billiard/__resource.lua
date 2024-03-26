5c3a50d59e8f330fee67c358cbdae814b5414ce7
frisck <55840140+nsilvan1@users.noreply.github.com>
2024-03-26 00:14:12 -0300

    12

---
+++ resources/[mapas]/[alameda_mapa]/patoche_billiard/__resource.lua
@@ -0,0 +1,7 @@
+--shared_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/desert_bat_list.ytyp'
+              
