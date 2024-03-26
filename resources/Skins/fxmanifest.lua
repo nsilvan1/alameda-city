de44372a2d9609630e7cd1a6be266746d4720a3f
frisck <55840140+nsilvan1@users.noreply.github.com>
2024-03-25 21:49:29 -0300

    4

---
+++ resources/Skins/fxmanifest.lua
@@ -0,0 +1,31 @@
+
+
+
+fx_version "bodacious"
+game "gta5"
+
+files {
+	"data/*",
+}
+
+data_file "PED_METADATA_FILE" "data/peds.meta"
+data_file "PED_METADATA_FILE" "data/peds1.meta"
+data_file "PED_METADATA_FILE" "data/peds2.meta"
+data_file "PED_METADATA_FILE" "data/peds3.meta"
+data_file "PED_METADATA_FILE" "data/peds4.meta"
+data_file "PED_METADATA_FILE" "data/peds5.meta"
+data_file "PED_METADATA_FILE" "data/peds6.meta"
+data_file "PED_METADATA_FILE" "data/peds7.meta"
+data_file "PED_METADATA_FILE" "data/peds8.meta"
+data_file "PED_METADATA_FILE" "data/peds9.meta"
+data_file "PED_METADATA_FILE" "data/peds10.meta"
+data_file "PED_METADATA_FILE" "data/peds11.meta"
+data_file "PED_METADATA_FILE" "data/peds12.meta"
+data_file "PED_METADATA_FILE" "data/peds13.meta"
+data_file "PED_METADATA_FILE" "data/peds14.meta"
+data_file "PED_METADATA_FILE" "data/peds15.meta"
+data_file "PED_METADATA_FILE" "data/peds16.meta"
+data_file "PED_METADATA_FILE" "data/peds17.meta"
+data_file "PED_METADATA_FILE" "data/peds18.meta"
+data_file "PED_METADATA_FILE" "data/peds19.meta"
+
