cf232a3fb75886abaeea9069b6bc04781d443be9
frisck <55840140+nsilvan1@users.noreply.github.com>
2024-03-25 23:01:24 -0300

    12

---
+++ resources/[mapas]/[mapas]/[details]/water-world/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Kevin_M16'
+description 'Water Control'
+version '1.0.0'
+
+replace_level_meta 'meta/gta5'
+files {
+    'meta/gta5.meta',
+    'meta/water.xml'
+}
