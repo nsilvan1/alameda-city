c1f214613b26529babbea7d513829210e1cb5491
frisck <55840140+nsilvan1@users.noreply.github.com>
2024-03-26 00:18:08 -0300

    12

---
+++ resources/[mapas]/[alameda_mapa]/Props_marto/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_prop_ch_casino_trophies.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_03.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_04.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_accs.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_dlc_int_02_ch.ytyp'
