f5e626de103d6376a42d6f1ec05e1723097794a8
frisck <55840140+nsilvan1@users.noreply.github.com>
2024-03-26 11:56:41 -0300

    police

---
+++ resources/[carros]/Police_Alameda/fxmanifest.lua
@@ -0,0 +1,48 @@
+client_script "@vrp/lib/lib.lua"
+
+fx_version 'adamant'
+game "gta5"
+
+client_script "names.lua"
+
+files {
+	"audio/*",
+	"audio/**/*",
+    "data/**/*",
+	"data/**",
+	"data/*",
+}
+
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"
+
+data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
+data_file "AUDIO_GAMEDATA" "audio/elegyx/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/elegyx/elegyx_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/hemisound/dlc_hemisound"
+data_file "AUDIO_GAMEDATA" "audio/hemisound/hemisound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/hemisound/hemisound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r35sound/dlc_r35sound"
+data_file "AUDIO_GAMEDATA" "audio/r35sound/r35sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r35sound/r35sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r34sound/dlc_r34sound"
+data_file "AUDIO_GAMEDATA" "audio/r34sound/r34sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r34sound/r34sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/v8sultanrs/dlc_v8sultanrs"
+data_file "AUDIO_GAMEDATA" "audio/v8sultanrs/sultanrsv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/v8sultanrs/sultanrsv8_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/cw2019/dlc_cw2019"
+data_file "AUDIO_GAMEDATA" "audio/cw2019/cw2019_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/cw2019/cw2019_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/s15sound/dlc_s15sound"
+data_file "AUDIO_GAMEDATA" "audio/s15sound/s15sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/s15sound/s15sound_sounds.dat"
