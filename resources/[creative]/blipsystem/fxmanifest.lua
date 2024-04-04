0a697731cc7d7707127830f5e77fe3f137b22652
frisck <55840140+nsilvan1@users.noreply.github.com>
2024-04-04 11:16:43 -0300

    .

--- resources/[creative]/blipsystem/fxmanifest.lua
+++ resources/[creative]/blipsystem/fxmanifest.lua
@@ -1,15 +1,14 @@
-
-
-
 fx_version "bodacious"
 game "gta5"
+lua54 "yes"
 
 client_scripts {
-	"@vrp/lib/utils.lua",
+	"@vrp/config/Native.lua",
+	"@vrp/lib/Utils.lua",
 	"client-side/*"
 }
 
 server_scripts {
-	"@vrp/lib/utils.lua",
+	"@vrp/lib/Utils.lua",
 	"server-side/*"
 }
