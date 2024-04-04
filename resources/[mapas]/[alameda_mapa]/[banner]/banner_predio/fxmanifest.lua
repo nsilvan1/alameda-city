12fe43d8eed6c45441c0cd228848c32ded84a010
frisck <55840140+nsilvan1@users.noreply.github.com>
2024-04-04 00:52:27 -0300

    1

---
+++ resources/[mapas]/[alameda_mapa]/[banner]/banner_predio/fxmanifest.lua
@@ -0,0 +1,7 @@
+--- Edited       ! Anderson Fabris
+--- Obtenha seus modelos exclusivos atrávez do Contato ou Discord: AndiGame#5670
+--- https://discord.gg/rSeXKNq
+
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
